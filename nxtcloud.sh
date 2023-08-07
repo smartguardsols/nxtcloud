c0c00e1201910fc9da8fc28966f8fa641c38bb26
Smartguard Solutions <128658804+smartguardsols@users.noreply.github.com>
2023-08-07 03:23:07 +0000

    nxtcloud.sh

--- nxtcloud.sh
+++ nxtcloud.sh
@@ -1,28 +1,17 @@
 #!/bin/bash
 
-# Get the absolute path to the data directory
-DATA_DIR=$(pwd)
-
-# Step 1: Create Docker volume for Nextcloud data
-VOLUME_NAME="nextcloud_aio_nextcloud_datadir"
-docker volume create \
-  --driver local \
-  --name "$VOLUME_NAME" \
-  --opt type=none \
-  --opt device="$DATA_DIR" \
-  --opt o=bind
-
-# Step 2: Run Nextcloud All-In-One Docker container
+# For Linux and without a web server or reverse proxy (like Apache, Nginx, Cloudflare Tunnel and else) already in place:
 sudo docker run \
-  --sig-proxy=false \
-  --name nextcloud-aio-mastercontainer \
-  --restart always \
-  --publish 80:80 \
-  --publish 8080:8080 \
-  --publish 8443:8443 \
-  --env APACHE_PORT=11000 \
-  --env APACHE_IP_BINDING=0.0.0.0 \
-  --volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config \
-  --volume "$DATA_DIR":/var/www/html \
-  --volume /var/run/docker.sock:/var/run/docker.sock:ro \
-  nextcloud/all-in-one:latest
+--sig-proxy=false \
+--name nextcloud-aio-mastercontainer \
+--restart always \
+--publish 80:80 \
+--publish 8080:8080 \
+--publish 8443:8443 \
+--env APACHE_PORT=11000 \
+--env APACHE_IP_BINDING=0.0.0.0 \
+--volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config \
+--volume ~/nxt/data:/var/www/html \
+--volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config \
+--volume /var/run/docker.sock:/var/run/docker.sock:ro \
+nextcloud/all-in-one:latest
