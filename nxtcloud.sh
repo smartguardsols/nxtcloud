5017a7976a9536fd1a4eb93c245d24b7fea6ce8f
Smartguard Solutions <128658804+smartguardsols@users.noreply.github.com>
2023-08-07 03:10:27 +0000

    nxtcloud.sh

--- nxtcloud.sh
+++ nxtcloud.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Get the absolute path to the data directory
+DATA_DIR=$(pwd)
+
 # Step 1: Create Docker volume for Nextcloud data
 docker volume create \
   --driver local \
   --name nextcloud_aio_nextcloud_datadir \
   --opt type=none \
-  --opt device=~/nxt/data \
+  --opt device="$DATA_DIR" \
   --opt o=bind
 
 # Step 2: Run Nextcloud All-In-One Docker container
@@ -14,9 +17,11 @@ sudo docker run \
   --name nextcloud-aio-mastercontainer \
   --restart always \
   --publish 80:80 \
-  --publish 8082:8080 \
+  --publish 8080:8080 \
   --publish 8443:8443 \
-  --volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config \
-  --volume ~/nxt/data:/var/www/html \
+  --env APACHE_PORT=11000 \
+  --env APACHE_IP_BINDING=0.0.0.0 \
+  --volume nextcloud_aio_nextcloud_datadir:/mnt/docker-aio-config \
+  --volume "$DATA_DIR":/var/www/html \
   --volume /var/run/docker.sock:/var/run/docker.sock:ro \
   nextcloud/all-in-one:latest
